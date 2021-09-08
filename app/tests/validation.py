01401c9fb431c858a4e35ed7d02e271e030db45d
RabidSheep55 <tharreaupierre@gmail.com>
2021-09-08 12:05:23 +0200

    change default schema to include `answer` and `response`

--- app/tests/validation.py
+++ app/tests/validation.py
@@ -2,32 +2,37 @@ import unittest
 
 from ..tools.validate import validate_request
 
+
 class TestSchemaValidation(unittest.TestCase):
     """
-        TestCase Class used to test the schema.
-        ---
-        Tests are used here to check that the schema is able to 
-        validate and reject request bodies that would otherwise 
-        cause the program to crash.
-        
-        It's best practise to write these tests first to get a 
-        kind of 'specification' for what your schema should 
-        accept or reject, and you should run these tests before 
-        committing your code to git.
+    TestCase Class used to test the schema.
+    ---
+    Tests are used here to check that the schema is able to
+    validate and reject request bodies that would otherwise
+    cause the program to crash.
 
-        Read the docs on how to use unittest here:
-        https://docs.python.org/3/library/unittest.html
+    It's best practise to write these tests first to get a
+    kind of 'specification' for what your schema should
+    accept or reject, and you should run these tests before
+    committing your code to git.
 
-        Use validate_request() to check your schema works as 
-        it should.
+    Read the docs on how to use unittest here:
+    https://docs.python.org/3/library/unittest.html
+
+    Use validate_request() to check your schema works as
+    it should.
     """
+
     def test_empty_request_body(self):
         body = {}
 
         validation_error = validate_request(body)
 
         self.assertNotEqual(validation_error, None)
-        self.assertEqual(validation_error.get("message"), "Schema threw an error when validating the request body.")
+        self.assertEqual(
+            validation_error.get("message"),
+            "Schema threw an error when validating the request body.",
+        )
 
     def test_invalid_grading_data(self):
         body = {"hello": "world"}
@@ -35,14 +40,18 @@ class TestSchemaValidation(unittest.TestCase):
         validation_error = validate_request(body)
 
         self.assertNotEqual(validation_error, None)
-        self.assertEqual(validation_error.get("message"), "Schema threw an error when validating the request body.")
+        self.assertEqual(
+            validation_error.get("message"),
+            "Schema threw an error when validating the request body.",
+        )
 
     def test_valid_grading_data(self):
-        body = {"example": "property"}
+        body = {"response": "", "answer": ""}
 
         validation_error = validate_request(body)
 
         self.assertEqual(validation_error, None)
+
 
 if __name__ == "__main__":
     unittest.main()
